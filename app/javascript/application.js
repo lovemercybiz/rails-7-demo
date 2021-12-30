516c92f461cb69c4116f3fa4be9b66116a4cefb1
Love Slaughter <lovemercybiz@gmail.com>
2021-12-29 21:06:09 -0800

    Added local time via JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// advance js without node being installed
 import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+import LocalTime from "local-time"
+LocalTime.start()
